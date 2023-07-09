fce49a91198d72e4171028251fee7bbd7f99f621
Ronald <vanheugten.ronald@gmail.com>
2023-07-09 15:31:58 +0200

    Update benchmark.php

--- benchmark.php
+++ benchmark.php
@@ -2,26 +2,46 @@
 
 class BenchMark {
 
-    private $marks = array('named' => array(), 'marks' => []);
+    private $marks = null;
+
+    /**
+     * fill array for first use
+     */
+    function __construct() {
+        $this->marks = array('named' => array(), 'marks' => []);
+    }
+
+    /**
+     * Remove everything
+     */
+    function __destruct() {
+        unset($this->marks);
+    }
 
     /**
      * mark
      * 
      * The mark is a time triggerpoint. When providing $name with a value the time will be added to the $marks['named'] array.
-     * everytime the mark function is run it checks if the first named value exists if not it will add $marks['named']['start']
-     * Everytime the mark function runs it updates the ['stop'] time
+     * FIRST and LAST are reserved names for this function. 
+     * everytime the mark function is run it checks if the first named value exists if not it will add $marks['named']['first']
+     * Everytime the mark function runs it updates the ['last'] time
      * @param type $name
      */
     function mark($name) {
+        //Create timestamp
         $time = microtime(true);
+
+        if (strtolower($name) == "first" OR strtolower($name) == "last") {
+            die("first and last are reserved words for this bensmark function!");
+        }
+        if (!isset($this->marks['named']['first'])) {
+            $this->marks['named']['first'] = $time;
+        }
         if (isset($name)) {
             $this->marks['named'][$name] = $time;
         }
-        if (!isset($this->marks['marks']['start'])) {
-            $this->marks['marks']['start'] = $time;
-        }
         $this->marks['marks'][] = $time;
-        $this->marks['marks']['stop'] = $time;
+        $this->marks['named']['last'] = $time;
     }
 
     /**
@@ -32,7 +52,7 @@ class BenchMark {
      * @param type $last
      * @return type
      */
-    function time_between($first = 'start', $last = 'stop') {
+    function time_between($first, $last) {
         if (isset($first) AND isset($last)) {
             return $this->marks['named'][$last] - $this->marks['named'][$first];
         } else {
@@ -72,11 +92,10 @@ class BenchMark {
         } else {
             $time = $this->total_time();
         }
-        $time = $time + 500;
 
         $dtF = new \DateTime('@0');
         $dtT = new \DateTime("@$time");
-        return $dtF->diff($dtT)->format('Process time: %a days, %h hours, %i minutes and %s seconds');
+        echo $dtF->diff($dtT)->format('%a days, %h hours, %i minutes and %s seconds');
     }
 
 }
